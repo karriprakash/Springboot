805145c54408dd6b4e38f34712d405d33c09f6bf
Karri Surya Venkata Prakash Rao <karriprakash342@gmail.com>
2022-02-04 03:30:12 +0530

    First commit

---
+++ SpringDataRest1/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+CREATE TABLE ALIEN ( aid int, aname varchar(50) , tech varchar(50) );
+INSERT INTO ALIEN values (101,'Navin','JAVA');
+INSERT INTO Alien values (102,'Kiran','Android');
+INSERT INTO Alien values (103,'Pravin','Python');
+INSERT INTO Alien values (104,'Komal','JAVA');
+INSERT INTO Alien values (105,'Navin','JAVA');
+INSERT INTO Alien values (106,'Rafia','Android');
